424e9a529c467d0a97145bf68140ddd93a21aa0f
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-06-27 07:14:53 +0200

    poc wip

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/**
+ * @see {@link https://eslint.org/}
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  extends: 'standard-with-typescript',
+  'parserOptions': {
+    project: './tsconfig.json'
+  },
+  "env": {
+    "browser": false,
+    "node": true,
+  }
+}
