8989fe9d3f6e76fc9191765212265d505660df9e
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-05-15 13:57:57 +0200

    docs
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -26,7 +26,7 @@ const testbedsPath = path.join(testRootPath, '_data', 'testbeds');
 (function () {
   const REQUIRES_INSTALL = [
     /* region functional tests */
-    // !! none of the following ACTUALLY REQUIRES an install - lock files are enough
+    // !! none of the following ACTUALLY REQUIRES an installation - lock files are enough
     // 'bundled-dependencies',
     // 'concurrent-versions',
     // 'dev-dependencies',
