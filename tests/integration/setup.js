d5359752b7a10c9bbc094614cd1626f1787bda8a
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-05-29 12:49:18 +0200

    dings
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -56,7 +56,8 @@ const testbedsPath = path.join(testRootPath, '_data', 'testbeds');
   for (const DIR of REQUIRES_INSTALL) {
     console.log('>>> setup with yarn:', DIR)
     const done = spawnSync(
-      'yarn', ['install'], {
+      // allow mutations - we cate for the SBOM, not the lock ...
+      'yarn', ['install', '--no-immutable'], {
         cwd: path.join(testbedsPath, DIR),
         stdio: 'inherit',
         shell: true
