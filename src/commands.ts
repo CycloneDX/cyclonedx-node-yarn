ea32bec31e6768ce6901102f282ca4d7b5d24550
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-04-04 13:27:24 +0200

    feat: shorthands
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/commands.ts
+++ src/commands.ts
@@ -72,20 +72,20 @@ export class MakeSbomCommand extends Command<CommandContext> {
   })
 
   specVersion = makeChoiceSwitch<SpecVersion>(
-    '--spec-version',
+    '--sv, --spec-version',
     Object.keys(SpecVersionDict).sort(),
     SpecVersion.v1dot6,
     'Which version of CycloneDX to use.'
   )
 
   outputFormat = makeChoiceSwitch<OutputFormat>(
-    '--output-format',
+    '--of, --output-format',
     Object.values(OutputFormat).sort(),
     OutputFormat.JSON,
     'Which output format to use.'
   )
 
-  outputFile = Option.String('--output-file', OutputStdOut, {
+  outputFile = Option.String('-o, --output-file', OutputStdOut, {
     description: 'Path to the output file.\n' +
         `Set to "${OutputStdOut}" to write to STDOUT.\n` +
         '(default: write to STDOUT)'
