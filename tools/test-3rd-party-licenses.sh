82ad7499c2a560bf517dda22624154b43d07b2a1
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-03-03 14:12:37 +0100

    chore(deps): flict ignore unknown
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/test-3rd-party-licenses.sh
+++ tools/test-3rd-party-licenses.sh
@@ -7,7 +7,7 @@ LICENSES_JSON="$1"
 
 OL="$(jq -r '.ol' "$LICENSES_JSON")"
 
-flict verify \
+flict verify -ip \
 -ol "$OL" \
 -il "$(jq -r '.ils | join(" AND ")' "$LICENSES_JSON")" \
 || EC=$?;
@@ -26,7 +26,7 @@ echo "ERROR: found license issues. lets see details..." >&2
 
 jq -r '.ils[]' "$LICENSES_JSON" | while read -r IL
 do
-  flict verify -ol "$OL" -il "$IL" >&2
+  flict verify -ip -ol "$OL" -il "$IL" >&2
 done
 
 exit $EC
